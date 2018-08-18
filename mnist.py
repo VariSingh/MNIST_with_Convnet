64bff3475b253925b403d877d9ef92a137a927be
Varinder Singh <varindersingh271990@gmail.com>
2018-08-18 22:00:53 +0530

    made code working

--- mnist.py
+++ mnist.py
@@ -1,7 +1,8 @@
 import tflearn as tl
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
-
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.estimator import regression
 #load data
 import tflearn.datasets.mnist as mnist
 X, Y, testX, testY = mnist.load_data(one_hot=True)
@@ -22,13 +23,15 @@ network = max_pool_2d(network,2)
 network = conv_2d(network,64,3, activation="relu")
 network = max_pool_2d(network,2)
 
-fully_connected = fully_connected(network,100,activation="relu")
-fully_connected = fully_connected(fully_connected,10,activation="softmax")
+network = local_response_normalization(network)
 
-regression = regression(fully_connected, optimizer="adam", learning_rate=0.001, loss="categorical_crossentropy", name="target")
+network = fully_connected(network,256,activation="relu")
+network = fully_connected(network,10,activation="softmax")
+
+regression = regression(network, optimizer="adam", learning_rate=0.001, loss="categorical_crossentropy", name="target")
 
 
 #Training
 
-model = tflearn.DNN(regression,tensorboard_verbose=0)
+model = tl.DNN(regression,tensorboard_verbose=0)
 model.fit(X,Y, n_epoch=20, validation_set=(testX,testY),show_metric=True, batch_size=100, run_id="convNet_model")
